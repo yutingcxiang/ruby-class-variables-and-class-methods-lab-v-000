13259e20184754afb92758872cc814f96f889899
Christine Xiang <yutingcxiang@gmail.com>
2018-07-12 07:40:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
       if @@genre_count.include?(genre)
         @@genre_count[genre] += 1
       else
-        @genre_count[genre] = 1
+        @@genre_count[genre] = 1
       end
     end
     return @@genre_count
